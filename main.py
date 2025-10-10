efb0acf6b36d3bef73ed8975c79f0f007c06cd9e
Dharsan <dharsan19082002@gmail.com>
2025-10-10 05:57:41 +0530

    added generate function

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, shuffle, randint
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -8,8 +27,17 @@ def save():
     email = email_input.get()
     password = password_input.get()
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}")
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the credentials entered/generated: \nEmail: {email}\nPassword: {password}")
+
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_input.delete(0, END)
+                email_input.delete(0, END)
+                password_input.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -36,10 +64,10 @@ website_input.grid(row=1, column=1, columnspan=2)
 website_input.focus()
 email_input = Entry(width=38)
 email_input.grid(row=2, column=1, columnspan=2)
-password_input = Entry(width=21)
+password_input = Entry(width=20)
 password_input.grid(row=3, column=1)
 
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
